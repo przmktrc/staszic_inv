4120bb9da4936a8be6593ce56f5fc8cc1c0038ef
Przemysław <przmktrc@gmail.com>
2022-05-14 11:38:01 +0200

    Fixed adding items
    Added ability to set 0 as numeric values
    Fixed a bug where ram_model was replaced by cpu_model

--- add_item.php
+++ add_item.php
@@ -21,62 +21,62 @@ function get_query_string()
     $key_string = "name";
     $value_string = "\"" . $_REQUEST["name"] . "\"";
 
-    if ($_REQUEST["description"])
+    if ($_REQUEST["description"] != "")
     {
         $key_string = $key_string . ", description";
         $value_string = $value_string . ", \"" . $_REQUEST["description"] . "\"";
     }
-    if ($_REQUEST["manufacturer"])
+    if ($_REQUEST["manufacturer"] != "")
     {
         $key_string = $key_string . ", manufacturer";
         $value_string = $value_string . ", \"" . $_REQUEST["manufacturer"] . "\"";
     }
-    if ($_REQUEST["location"])
+    if ($_REQUEST["location"] != "")
     {
         $key_string = $key_string . ", location";
         $value_string = $value_string . ", \"" . $_REQUEST["location"] . "\"";
     }
-    if ($_REQUEST["cpu_model"])
+    if ($_REQUEST["cpu_model"] != "")
     {
         $key_string = $key_string . ", cpu_model";
         $value_string = $value_string . ", \"" . $_REQUEST["cpu_model"] . "\"";
     }
-    if ($_REQUEST["cpu_max_freq_mhz"])
+    if ($_REQUEST["cpu_max_freq_mhz"] != "")
     {
         $key_string = $key_string . ", cpu_max_freq_mhz";
         $value_string = $value_string . ", " . $_REQUEST["cpu_max_freq_mhz"];
     }
-    if ($_REQUEST["ram_model"])
+    if ($_REQUEST["ram_model"] != "")
     {
-        $key_string = $key_string . ", cpu_model";
+        $key_string = $key_string . ", ram_model";
         $value_string = $value_string . ", \"" . $_REQUEST["ram_model"] . "\"";
     }
-    if ($_REQUEST["ram_amount_gb"])
+    if ($_REQUEST["ram_amount_gb"] != "")
     {
         $key_string = $key_string . ", ram_amount_gb";
         $value_string = $value_string . ", " . $_REQUEST["ram_amount_gb"];
     }
-    if ($_REQUEST["graphics_model"])
+    if ($_REQUEST["graphics_model"] != "")
     {
         $key_string = $key_string . ", graphics_model";
         $value_string = $value_string . ", \"" . $_REQUEST["graphics_model"] . "\"";
     }
-    if ($_REQUEST["disk_model"])
+    if ($_REQUEST["disk_model"] != "")
     {
         $key_string = $key_string . ", disk_model";
         $value_string = $value_string . ", \"" . $_REQUEST["disk_model"] . "\"";
     }
-    if ($_REQUEST["disk_size_gb"])
+    if ($_REQUEST["disk_size_gb"] != "")
     {
         $key_string = $key_string . ", disk_size_gb";
         $value_string = $value_string . ", " . $_REQUEST["disk_size_gb"];
     }
-    if ($_REQUEST["screen_diagonal_inch"])
+    if ($_REQUEST["screen_diagonal_inch"] != "")
     {
         $key_string = $key_string . ", screen_diagonal_inch";
         $value_string = $value_string . ", " . $_REQUEST["screen_diagonal_inch"];
     }
-    if ($_REQUEST["screen_resolution"])
+    if ($_REQUEST["screen_resolution"] != "")
     {
         $key_string = $key_string . ", screen_resolution";
         $value_string = $value_string . ", \"" . $_REQUEST["screen_resolution"] . "\"";
@@ -98,6 +98,8 @@ if ($_REQUEST["name"])
             echo "Failed to connect to database but no exception was thrown.<br>";
 
         $query_string = get_query_string();
+        echo "query_string: " . $query_string . "<br>";
+
         if ($query_string)
             $query_result_pdo = $pdo->query($query_string);
     }
